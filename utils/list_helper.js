17063364f256dc0d2555eba435aefd096185a0ba
Facu Arrascaeta <arrascaetafacu@outlook.com>
2022-12-02 14:38:12 -0300

    Exersises 4.3-4.5 unit tests

---
+++ utils/list_helper.js
@@ -0,0 +1,26 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => sum + blog.likes
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  let favorite = blogs[0]
+  blogs.forEach(blog => {
+    if (blog.likes > favorite.likes) favorite = blog
+  })
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
+module.exports = {
+  dummy, totalLikes, favoriteBlog
+}
